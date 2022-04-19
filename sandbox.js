d955188bc1268a40a1eceaad07d0e5a187cac153
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-19 22:51:27 +0100

    Explicit Types

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,13 @@
 // explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'plankton';
+age = 30;
+// isLoggedIn = 34;
+isLoggedIn = true;
 // arrays
+var ninjas = [];
+ninjas.push('warwick');
 // union types
 // objects
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,19 @@
 // explicit types
 let character: string;
 let age: number;
-let isLogging: boolean
+let isLoggedIn: boolean
+
+// age = 'plankton';
+age = 30;
+
+// isLoggedIn = 34;
+isLoggedIn = true;
+
 
 // arrays
+let ninjas: string[] = [];
+
+ninjas.push('warwick');
 
 
 // union types
