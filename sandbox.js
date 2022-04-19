83c1c33535eacc7b23cbcded57965a100ee55056
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-19 22:46:19 +0100

    Explicit Types

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,4 @@
-//ararys 
-var names = ['patrick', 'sandy', 'krabbs'];
-names.push('spongebob');
-//names.push(54);
-// nmaes[0] = 8;
-var numbers = [10, 20, 40, 3];
-// numbers.push('weird');
-// numbers[1] = 'weird';
-var mixed = ['ken', 4, 'gary', 6, 9];
-mixed.push('lary');
-mixed.push(10);
-mixed[0] = 3;
+// explicit types
+// arrays
+// union types
 // objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 45
-};
-ninja.age = 20;
-ninja.name = 'chun-li';
-//ninja.age = '40'
--- sandbox.ts
+++ sandbox.ts
@@ -1,28 +1,10 @@
-//ararys 
-let names = ['patrick', 'sandy', 'krabbs']
+// explicit types
 
-names.push('spongebob');
-//names.push(54);
-// nmaes[0] = 8;
 
-let numbers = [10, 20, 40, 3];
-// numbers.push('weird');
-// numbers[1] = 'weird';
+// arrays
 
-let mixed = ['ken', 4, 'gary', 6, 9];
 
-mixed.push('lary');
-mixed.push(10);
-mixed[0] = 3;
+// union types
+
 
 // objects
-
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 45
-}
-
-ninja.age = 20;
-ninja.name = 'chun-li';
-//ninja.age = '40'
