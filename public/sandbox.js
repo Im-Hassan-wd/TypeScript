2086b6ce982bc69b5b009464994b761302680ba3
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-20 05:02:28 +0100

    function signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,5 +1,9 @@
 "use strict";
-let greet;
+//let greet: Function;
 // example 1
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
 // example 2
 // example 3
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,12 +1,13 @@
-let greet: Function
+//let greet: Function;
 
 
 // example 1
-
-
+let greet: (a: string, b: string) => void
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+}
 
 // example 2
-
 
 
 // example 3
