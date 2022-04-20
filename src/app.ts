6a684d9ddb0bd407ffcf86509396d9e6dd20f1c6
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-20 19:41:37 +0100

    DOM & type casting

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-const anchor = document.querySelector('a');
+// const anchor = document.querySelector('a')!;
 
 // if(anchor) {
 //   console.log(anchor.href)
 // }
+
+// const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children)
