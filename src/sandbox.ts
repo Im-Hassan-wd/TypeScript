84fff41add3da15c1ee7c575cdcb042299151640
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-20 04:37:29 +0100

    Function basics

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,3 +5,10 @@ let greet: Function;
 greet = () => {
   console.log('hello, again');
 }
+
+const add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b)
+  console.log(c)
+}
+
+add(5, 10, '20')
