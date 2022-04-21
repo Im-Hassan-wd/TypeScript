d1d933e01a9a9879e89d6e60fb491d54bb8cadb0
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-21 05:27:37 +0100

    rendering an HTMl template

---
+++ src/classes/ListTemplate.ts
@@ -0,0 +1,8 @@
+
+/*
+  1. register a list (ul) in the constuctor
+  2. create a render method to render a new 'li' to the container
+      --- accepts argument: invoice or payment, a heading, a position
+      --- create the html template (li, h4, p)
+      --- add the 'li' template to the start/end of the list
+*/
