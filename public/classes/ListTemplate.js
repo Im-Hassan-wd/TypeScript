b0baa27fb2ed59a8df40897b5976c5ea4a44ff79
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-21 05:35:13 +0100

    rendering an HTMl template

--- public/classes/ListTemplate.js
+++ public/classes/ListTemplate.js
@@ -1,4 +1,23 @@
-"use strict";
+export class ListTemplate {
+    constructor(container) {
+        this.container = container;
+    }
+    render(item, heading, pos) {
+        const li = document.createElement('li');
+        const h4 = document.createElement('h4');
+        h4.innerText = heading;
+        li.append(h4);
+        const p = document.createElement('p');
+        p.innerText = item.format();
+        li.append(p);
+        if (pos === 'start') {
+            this.container.prepend(li);
+        }
+        else {
+            this.container.append(li);
+        }
+    }
+}
 /*
   1. register a list (ul) in the constuctor
   2. create a render method to render a new 'li' to the container
