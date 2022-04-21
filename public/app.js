b28942e73671fc3dc415252111c28ed499d7c1c6
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-21 05:21:36 +0100

    Interfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,21 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs)
-// const invOne = new Invoice('patrick', 'work in the mario website', 250);
-// const invTwo = new Invoice('gary', 'work in the gary website', 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne)
-// invoices.push(invTwo)
-// invoices.forEach(inv => {
-//   console.log(inv.client, inv.amount, inv.format())
-// })
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
